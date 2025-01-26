cc2f7af8684468d9db2e5f4b62da235f438e4e29
technicolor-dreams <matthew@Matts-MacBook-Pro.local>
2025-01-26 14:07:20 -0500

    3 top interview questions

---
+++ java/src/main/java/io/github/technicolordreams/top/interview/questions/hashmap/ContainsDuplicateII.java
@@ -0,0 +1,33 @@
+package io.github.technicolordreams.top.interview.questions.hashmap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * <h1><a href="https://leetcode.com/problems/contains-duplicate-ii/description/">219. Contains Duplicate II</a></h1>
+ * <h2>Easy</h2>
+ * <h3>2025-01-26</h3>
+ */
+public class ContainsDuplicateII {
+    public boolean solution(int[] nums, int k) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int finalI = i;
+            int cur = nums[i];
+            map.putIfAbsent(cur, new HashSet<>());
+
+            if (map.get(cur).stream().anyMatch(j -> Math.abs(finalI - j) <= k)) {
+                return true;
+            }
+
+            map.computeIfPresent(cur, (key, v) -> {
+                v.add(finalI);
+                return v;
+            });
+        }
+
+        return false;
+    }
+}
