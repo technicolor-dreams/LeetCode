4b01e91c4b238434ecf79c59da60bb8e0c425c8e
technicolor-dreams <matthew@Matts-MacBook-Pro.local>
2025-01-27 10:47:22 -0500

    2 top interview questions

---
+++ java/src/main/java/io/github/technicolordreams/top/interview/questions/linked/list/ListNode.java
@@ -0,0 +1,10 @@
+package io.github.technicolordreams.top.interview.questions.linked.list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        this.val = x;
+        this.next = null;
+    }
+}
---
+++ java/src/main/java/io/github/technicolordreams/top/interview/questions/stack/ValidParentheses.java
@@ -0,0 +1,41 @@
+package io.github.technicolordreams.top.interview.questions.stack;
+
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * <h1><a href="https://leetcode.com/problems/valid-parentheses/description/">20. Valid Parentheses</a></h1>
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        ValidParentheses validParentheses = new ValidParentheses();
+//        System.out.println(validParentheses.solution("()"));
+//        System.out.println(validParentheses.solution("(){}[]"));
+//        System.out.println(validParentheses.solution("(]"));
+//        System.out.println(validParentheses.solution("([)]"));
+        System.out.println(validParentheses.solution("({[)"));
+    }
+
+    public boolean solution(String s) {
+        Map<Character, Character> validParentheses = Map.of('(', ')', '{', '}', '[', ']');
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.empty()) {
+                stack.push(c);
+                continue;
+            }
+
+            if (validParentheses.containsKey(stack.peek())) {
+                if (c == validParentheses.get(stack.peek())) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+}
