9a9e5b4dfc02e8cb57b883ce07c07c35a99f2378
technicolor-dreams <matthew@Matts-MacBook-Pro.local>
2025-02-14 13:11:31 -0500

    Question of the day

---
+++ java/src/main/java/io/github/technicolordreams/question/of/the/day/medium/ProductOfNumbers.java
@@ -0,0 +1,36 @@
+package io.github.technicolordreams.question.of.the.day.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h1><a href="https://leetcode.com/problems/product-of-the-last-k-numbers/description/">1352. Product of the Last K Numbers</a></h1>
+ * <h2>Medium</h2>
+ * <h3>2025-02-14</h3>
+ */
+public class ProductOfNumbers {
+
+    private final List<Integer> prefix;
+
+    public ProductOfNumbers() {
+        this.prefix = new ArrayList<>();
+        this.prefix.add(1);
+    }
+
+    public void add(int num) {
+        if (num == 0) {
+            this.prefix.clear();
+            this.prefix.add(1);
+        } else {
+            this.prefix.add(this.prefix.getLast() * num);
+        }
+    }
+
+    public int getProduct(int k) {
+        int size = this.prefix.size();
+        if (k >= size) {
+            return 0; // If k is larger than available numbers (due to zero reset)
+        }
+        return this.prefix.get(size - 1) / this.prefix.get(size - k - 1);
+    }
+}
