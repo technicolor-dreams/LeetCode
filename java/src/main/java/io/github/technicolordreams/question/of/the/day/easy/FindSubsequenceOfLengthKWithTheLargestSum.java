821e4d1cfafe988e3d1156f113af0dc690b3bec2
matthew-jay-wong <matthew.jay.wong@gmail.com>
2025-06-28 13:59:44 -0400

    Question of the day: Optimize space and simplify with streams

--- java/src/main/java/io/github/technicolordreams/question/of/the/day/easy/FindSubsequenceOfLengthKWithTheLargestSum.java
+++ java/src/main/java/io/github/technicolordreams/question/of/the/day/easy/FindSubsequenceOfLengthKWithTheLargestSum.java
@@ -14,40 +14,30 @@ public class FindSubsequenceOfLengthKWithTheLargestSum {
 
     public static void main(String[] args) {
         FindSubsequenceOfLengthKWithTheLargestSum a = new FindSubsequenceOfLengthKWithTheLargestSum();
-        assert Arrays.equals(a.solution(new int[]{2, 1, 3, 3}, 2), new int[]{3, 3});
-        assert Arrays.equals(a.solution(new int[]{-1, -2, 3, 4}, 3), new int[]{-1, 3, 4});
-        assert Arrays.equals(a.solution(new int[]{3, 4, 3, 3}, 2), new int[]{3, 4});
+        assert Arrays.equals(a.solution(new int[]{2, 1, 3, 3}, 2), new int[]{3, 3}) : "Fail";
+        assert Arrays.equals(a.solution(new int[]{-1, -2, 3, 4}, 3), new int[]{-1, 3, 4}) : "Fail";
+        assert Arrays.equals(a.solution(new int[]{3, 4, 3, 3}, 2), new int[]{3, 4}) : "Fail";
     }
 
     public int[] solution(int[] nums, int k) {
+
         PriorityQueue<Map.Entry<Integer, Integer>> priorityQueueVal =
                 new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+
         for (int i = 0; i < nums.length; i += 1) {
             int cur = nums[i];
             if (priorityQueueVal.size() < k) {
                 priorityQueueVal.add(Map.entry(i, cur));
-            } else {
-                Map.Entry<Integer, Integer> curMin = priorityQueueVal.poll();
-                if (curMin.getValue() < cur) {
-                    priorityQueueVal.add(Map.entry(i, cur));
-                } else {
-                    priorityQueueVal.add(curMin);
-
-                }
+            } else if (priorityQueueVal.peek().getValue() < cur) {
+                priorityQueueVal.poll();
+                priorityQueueVal.add(Map.entry(i, cur));
             }
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> priorityQueueKey =
-                new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getKey));
-        while (!priorityQueueVal.isEmpty()) {
-            priorityQueueKey.add(priorityQueueVal.poll());
-        }
-
-        int[] res = new int[k];
-        for (int i = 0; i < k; i += 1) {
-            res[i] = priorityQueueKey.poll().getValue();
-        }
-
-        return res;
+        return priorityQueueVal
+                .stream()
+                .sorted(Comparator.comparingInt(Map.Entry::getKey))
+                .mapToInt(Map.Entry::getValue)
+                .toArray();
     }
 }
